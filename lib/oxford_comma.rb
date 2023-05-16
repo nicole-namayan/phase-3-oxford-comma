66ff0f40e5daa446fb8a57002b9415846306c6d6
nicole <nicolenamayan133@gmail.com>
2023-05-16 20:12:55 +0300

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+     case array.length
+     when 0
+       ""
+     when 1
+    array[0]
+     when 2
+   "#{array[0]} and #{array[1]}"
+    else
+   last_element = array.pop
+ "#{array.join(", ")}, and #{last_element}"
+end     
 end
